fd2def8f1aa2d71f89d3f9b64814ab8b1c71dcab
RinseWillet <r.willet@gmail.com>
2021-04-30 17:57:40 +0200

    stap 3

--- mapscript.js
+++ mapscript.js
@@ -1,1 +1,29 @@
 // hier komt alle scripting voor de kaart
+
+// We gaan naar Mapbox website 
+
+// Waarom Mapbox? Mooi, maar ook geen creditcard nodig in beginsel 
+
+// Laat code zien (access code – die heb je later nodig) 
+
+// MapBox Studio -> gebruiken om eigen kaarten te maken of bestaande (standaard) profielen aan te passen 
+
+// Gallery laten zien 
+
+// Begin met een blank style 
+
+// Add land water, & Sky en pas de kleuren aan 
+
+// Zorg dat colors managed for Base, Greenspace, Roads, Road Outline, Water 
+
+// Check ICONS 
+
+// Check opacity 
+
+// Ga lekker door tot je een redelijk resultaat hebt, dan publish 
+
+// En dan Share en Develop, Third Party, CARTO – die link moet je kopiëren 
+
+
+//LINK NAAR JE MAPBOX STUDIO CREATIE
+‘https://api.mapbox.com/styles/v1/USERNAME/STYLE_ID_NUMMER/tiles/256/{z}/{x}/{y}@2x?access_token=JOUW_API_KEY’ 
